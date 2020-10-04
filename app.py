e148d7786f74d37d162f1b94e140392b933d415d
raphaelmar <raphaelmar@hotmail.fr>
2020-10-04 20:43:41 +0000

    added addtask.html

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
 app.config["MONGO_URI"] = "mongodb+srv://Raphaelmar:CestReLou@myfirstcluster.goeyw.mongodb.net/task_manager?retryWrites=true&w=majority"
-
 
 
 mongo = PyMongo(app)
@@ -16,6 +15,11 @@ mongo = PyMongo(app)
 @app.route('/get_tasks')
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
 
 
 if __name__ == '__main__':
