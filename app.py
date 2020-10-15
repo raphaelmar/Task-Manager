2500bf046415143772b08f61e6fd0220a252b843
raphaelmar <raphaelmar@hotmail.fr>
2020-10-15 09:52:42 +0000

    added insert tasks

--- app.py
+++ app.py
@@ -10,19 +10,27 @@ app.config["MONGO_URI"] = "mongodb+srv://Raphaelmar:CestReLou@myfirstcluster.goe
 
 mongo = PyMongo(app)
 
-
 @app.route('/')
-@app.route('/get_tasks')
-def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
-
 
 @app.route('/add_task')
 def add_task():
-    return render_template('addtask.html')
+    return render_template('addtask.html',
+                           categories=mongo.db.categories.find())
+
+
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", 
+                           tasks=mongo.db.tasks.find())
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=True)
+        port=int(os.environ.get('PORT')),
+        debug=True)
